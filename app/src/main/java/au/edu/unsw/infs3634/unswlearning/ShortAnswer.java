89f6437fa1df8cf78fac132b0d65b5081c720774
kellytrangdao <z5261905@student.unsw.edu.au>
2022-10-23 14:55:12 +1100

    Created short answer quiz table
    Need to insert Image View to the table and add image to the List

--- app/src/main/java/au/edu/unsw/infs3634/unswlearning/ShortAnswer.java
+++ app/src/main/java/au/edu/unsw/infs3634/unswlearning/ShortAnswer.java
@@ -5,15 +5,16 @@ import android.widget.ImageView;
 public class ShortAnswer extends Question{
     private String userAnswer;
     private ImageView questionImage; //NOT NULL
+    private String questionText;
 
     //Setter methods
-    public void setUserAnswer(String userAnswer) {
-        this.userAnswer = userAnswer;
-    }
+    public void setUserAnswer(String userAnswer) { this.userAnswer = userAnswer; }
 
     public void setQuestionImage(ImageView questionImage) {
         this.questionImage = questionImage;
     }
+
+    public void setQuestionText(String questionText) {this.questionText = questionText; }
 
     //Getter methods
     public String getUserAnswer(){
@@ -23,4 +24,6 @@ public class ShortAnswer extends Question{
     public ImageView getQuestionImage() {
         return questionImage;
     }
+
+    public String getQuestionText() { return questionText; }
 }
