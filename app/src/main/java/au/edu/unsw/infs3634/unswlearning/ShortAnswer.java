8d3d7cadd8913f36ae95bf3b94b3582e20b1aab8
kellytrangdao <z5261905@student.unsw.edu.au>
2022-11-03 15:41:14 +1100

    check answer for SAQ

--- app/src/main/java/au/edu/unsw/infs3634/unswlearning/ShortAnswer.java
+++ app/src/main/java/au/edu/unsw/infs3634/unswlearning/ShortAnswer.java
@@ -5,18 +5,27 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 public class ShortAnswer extends Question{
-        
+
     private String userAnswer;
+    private String questionText;
     private ImageView questionImage; //NOT NULL
-//Constructor
-    public ShortAnswer(int id, String region, String textQuestion, String answer,
-                       String userAnswer, ImageView questionImage) {
+
+    //Constructor
+    public ShortAnswer(int id, String region, String textQuestion, String answer) {
         super(id, region, textQuestion, answer);
         this.userAnswer = userAnswer;
+        this.questionText = questionText;
         this.questionImage = questionImage;
     }
+
+    public ShortAnswer(int anInt, String rsString1, String string1, String s, String rsString, String string, String userAnswer, String questionText) {
+
+    }
+
     //Setter methods
     public void setUserAnswer(String userAnswer) { this.userAnswer = userAnswer; }
+
+    public void setQuestionText(String questionText) { this.questionText = questionText; }
 
     public void setQuestionImage(ImageView questionImage) {
         this.questionImage = questionImage;
@@ -25,6 +34,10 @@ public class ShortAnswer extends Question{
     //Getter methods
     public String getUserAnswer(){
         return userAnswer;
+    }
+
+    public String getQuestionText(){
+        return questionText;
     }
 
     public ImageView getQuestionImage() {
--- app/src/main/java/au/edu/unsw/infs3634/unswlearning/ShortAnswerQuizActivity.java
+++ app/src/main/java/au/edu/unsw/infs3634/unswlearning/ShortAnswerQuizActivity.java
@@ -1,15 +1,22 @@
 package au.edu.unsw.infs3634.unswlearning;
 
+import static java.util.Arrays.*;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
@@ -30,6 +37,7 @@ public class ShortAnswerQuizActivity extends AppCompatActivity {
     private ShortAnswer currentQuestion;
     private boolean answered;
     private int quizScore;
+    private int curQuestion = 0;
 
     public static String regionChosen;
 
@@ -47,15 +55,47 @@ public class ShortAnswerQuizActivity extends AppCompatActivity {
         userAnswerET = findViewById(R.id.userAnswerET);
         submitAnswerButton = findViewById(R.id.submitAnswerButton);
 
+        //Take the region name from QuizActivity screen
+        Intent intent = getIntent();
+        //prevent app from crashing
+        //if the intent has been passed through successfully
+        if (intent.hasExtra(regionChosen)) {
+            //Get message passed from the MainActivity
+            String message = intent.getStringExtra(regionChosen);
+            //Update text with message
+            quizRegionTV.setText(regionChosen);
+        }
+
+        shortAnswerQuestionList = new ArrayList<>();
+
+        // Add all questions and answers to the quiz
         try {
-            shortAnswerQuestionList = ShortAnswerDatabase.getShortAnswerQuestion();
+            shortAnswerQuestionList = ShortAnswerDatabase.getShortAnswerQuestionByRegion(regionChosen);
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
-        //Shuffle the question list
+        //Shuffle the questions
         Collections.shuffle(shortAnswerQuestionList);
         showNextQuestion();
+
+        String userAnswer = userAnswerET.getText().toString();
+
+        // Handle submit button
+        submitAnswerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!answered) {
+                    if (userAnswer.matches("")) {
+                        Toast.makeText(ShortAnswerQuizActivity.this, "Please enter your answer", Toast.LENGTH_SHORT).show();
+                    } else {
+                        checkAnswer();
+                    }
+                } else {
+                    showNextQuestion();
+                }
+            }
+        });
     }
 
     private void showNextQuestion() {
@@ -85,13 +125,14 @@ public class ShortAnswerQuizActivity extends AppCompatActivity {
 
         String userAnswer = userAnswerET.getText().toString();
 
-        // Compare userAnswer with the correct answer
-        if (userAnswer == currentQuestion.getUserAnswer()) {
+        // check if the answer is correct
+        if (userAnswer.equalsIgnoreCase(currentQuestion.getAnswer())) {
             quizScore++;
             quizScoreTV.setText("Score: " + quizScore);
         }
-
-        showCorrectAnswer();
+        else {
+            showCorrectAnswer();
+        }
 
     }
 
@@ -100,6 +141,12 @@ public class ShortAnswerQuizActivity extends AppCompatActivity {
     }
 
     private void finishQuiz() {
+        //If user gets 5/5 correct answers, add 10 points to Total Points
+        if (quizScore == questionCount) {
+            User currentUser = new User();
+            currentUser.addPoints();
+            currentUser.addLevelPassed();
+        }
         finish();
     }
 }
